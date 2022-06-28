4b448db527d239cb735cb7219d3c707a841d127a
marat <mrxpro2003@gmail.com>
2022-06-28 17:51:04 +0500

    Pagination

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="news">
+        <div class="news__top">
+            <div class="news__list" v-for="(newmax,index) in newsmax" :key="index">
+                <img :src="newmax.img" :alt="newmax.img">
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<script>
+export default {
+    props:['newsmax']
+}
+</script>
---
+++ src/data/index.js
@@ -0,0 +1,41 @@
+import page from '../assets/foto/page.png'
+import page1 from '../assets/foto/page1.png'
+import page2 from '../assets/foto/page2.png'
+import page3 from '../assets/foto/page3.png'
+import page4 from '../assets/foto/page4.png'
+import page5 from '../assets/foto/page5.png'
+import page6 from '../assets/foto/page6.png'
+
+export default[
+    {
+        img:page,
+    },
+
+    {
+        img:page1,
+    },
+
+    {
+        img:page2,
+    },
+
+    {
+        img:page3,
+    },
+
+    {
+        img:page4,
+    },
+
+    {
+        img:page5,
+    },
+    
+    {
+        img:page6,
+    },
+
+    {
+        img:page,
+    },
+]
